8dba015f95df8bb5e2f8443b80a56f3e08e96351
rajneeshb <rajneeshbabu@ldiinteractive.com>
2012-05-24 17:16:36 +0530

    change settings file

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -165,6 +165,7 @@ ini_set('session.use_cookies',      1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
+ini_set('memory_limit',        '256M');
 
 /**
  * If you encounter a situation where users post a large amount of text, and
